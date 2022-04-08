d06616a4a5ef5f93f3525668c06074ed3b0175ee
Kenny Clare <kmitchellclare@gmail.com>
2022-04-08 16:30:40 -0700

    commit tree d3f22c38b8c733b8c0eb79601357b8186420aab9

---
+++ index.js
@@ -0,0 +1,60 @@
+function add(a, b,) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+let n = 1;
+function increment(n) {
+    n++;
+    return n;
+}
+
+let n2 = 2;
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// //
+// let number = 5;
+
+// number++; //=> 5...
+
+// number; //=> 6 - - the number was incremented after it was evaluated
+
+// number--; //=> 6
+
+// number; //=> 5
+
+
